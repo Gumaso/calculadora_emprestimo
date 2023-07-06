ba4627f42e0b84470ac7e735e452aab84c77c65c
Gustavo <gustavomacedosousa013@gmail.com>
2023-07-06 20:59:34 -0300

    Criando nosso projeto na primeira etapa, mensagens exibidas

---
+++ calculadora.py
@@ -0,0 +1,5 @@
+from math import ceil
+mensagens = ["Empréstimo principal efetivado: R$1000", "Mês 1: pago R$250", "Mês 2: pago R$250", "Mês 3: pago R$500"]
+for msg in mensagens:
+    print(msg)
+print("Empréstimo efetuado ")
